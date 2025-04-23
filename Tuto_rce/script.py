05e1f88a27091089a3c7b96e9daae0063087c6fb
Romain <romain.pietri@student.junia.com>
2025-04-23 18:32:12 +0300

    Test avec ces dockerfiles

--- Tuto_rce/script.py
+++ Tuto_rce/script.py
@@ -57,7 +57,7 @@ try :
     
     os.chdir(docker_compose_dir)  # Changer le répertoire de travail
     run("docker-compose down")
-    run("docker-compose build")
+    run("docker-compose  build")
     run("docker-compose up -d")
 except Exception as e:
     print(f"Erreur lors du démarrage des conteneurs : {e}")
